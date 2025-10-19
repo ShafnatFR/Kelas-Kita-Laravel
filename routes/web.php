a3c31b04d5a4364a280f0366449b2b3c92fbb5eb
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2025-10-20 06:23:53 +0700

    Edit

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ use App\Http\Controllers\kelolaUser;
 Route::get('/', [kelolaUser::class, 'showUser'])->name('dashboardAdmin');
 
 Route::post('/createUser', [kelolaUser::class, 'createUser'])->name('createUser.post');
+
 Route::get('/createUserView', function () {
     return view('createUser');
 });
+
+Route::get('/edit', function () {
+    return view('editUser', ['id' => request('id')]);
+})->name('editUser.view');
