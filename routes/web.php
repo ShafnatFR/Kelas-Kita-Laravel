96e70e105740471dac527f458e5ef4dcefdc2595
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2025-10-20 12:07:42 +0700

    Restart

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\kelolaUser;
 
 Route::get('/', [kelolaUser::class, 'showUser'])->name('dashboardAdmin');
-
-Route::post('/createUser', [kelolaUser::class, 'createUser'])->name('createUser.post');
-
-Route::get('/createUserView', function () {
-    return view('createUser');
-});
-
-Route::get('/edit', function () {
-    return view('editUser', ['id' => request('id')]);
-})->name('editUser.view');
